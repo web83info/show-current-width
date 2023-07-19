5937946b55b9e9f5f6398c4c6d181b2b8b993514
Web83 <me@web83.info>
2023-07-19 12:44:01 +0900

    Fix: Multisite uninstall
    When multisite, uninstall options on all sub sites.

--- uninstall.php
+++ uninstall.php
@@ -10,9 +10,32 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 // Delete all options if option 'w83-show-current-width_other_uninstall' is checked.
-if ( '1' === get_option( 'w83-show-current-width_other_uninstall' ) ) {
-	// Delete all options whose key begins with 'w83-show-current-width_'.
-	foreach ( wp_load_alloptions( true ) as $option_key => $option_value ) {
+if ( is_multisite() ) {
+	global $wpdb;
+	$blogid_current = get_current_blog_id();
+	$blogids        = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+	foreach ( $blogids as $blogid ) {
+		switch_to_blog( $blogid );
+		if ( '1' === get_option( 'w83-show-current-width_other_uninstall' ) ) {
+			// Delete all options whose key begins with 'w83-show-current-width_'.
+			delete_all_options( wp_load_alloptions( true ) );
+		}
+	}
+	switch_to_blog( $blogid_current );
+} else {
+	if ( '1' === get_option( 'w83-show-current-width_other_uninstall' ) ) {
+		// Delete all options whose key begins with 'w83-show-current-width_'.
+		delete_all_options( wp_load_alloptions( true ) );
+	}
+}
+
+/**
+ * Delete all options core.
+ *
+ * @param array $alloptions Load all options.
+ */
+function delete_all_options( $alloptions ) {
+	foreach ( $alloptions as $option_key => $option_value ) {
 		if ( false !== strpos( $option_key, 'w83-show-current-width_' ) ) {
 			delete_option( $option_key );
 		}
